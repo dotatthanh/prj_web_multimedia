0896eb7b3987af6812765ed88f0137a42d586c72
unknown <thangbui.071192@gmail.com>
2021-12-08 14:11:48 +0700

    add info seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,5 +16,6 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
         $this->call(UserSeeder::class);
         $this->call(RoleAndPermissionSeeder::class);
+        $this->call(InfoSeeder::class);
     }
 }
