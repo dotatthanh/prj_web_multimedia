1d62a5f4e57a06a18ebc4f37808f50e87c99c0b3
unknown <thangbui.071192@gmail.com>
2021-12-21 19:10:58 +0700

    fix seed

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,7 +4,6 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\User;
-use DB;
 
 class UserSeeder extends Seeder
 {
@@ -15,29 +14,18 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-
-        DB::table('users')->truncate();
-        DB::table('users')->insert([
-            'code' => 'SUPER ADMIN',
-            'email' => 'ducthang.dt03@gmail.com',
-            'password' => bcrypt('123123123'),
-            'name' => 'Super Admin',
-            'birthday' => '1999-03-21',
-            'phone_number' => '0563047024',
-            'address' => 'Đại học kiến trúc Hà Nội',
-            'gender' => 'Nam',
-        ]);
         // // Tạo admin
-        // User::create([
-        //     'id' => 1,
-        //     'code' => 'SUPER ADMIN',
-        // 	'email' => 'ducthang.dt03@gmail.com',
-        // 	'password' => bcrypt('123123123'),
-        // 	'name' => 'Super Admin',
-        // 	'birthday' => '1999-03-21',
-        // 	'phone_number' => '0563047024',
-        // 	'address' => 'Đại học kiến trúc Hà Nội',
-        // 	'gender' => 'Nam',
-        // ]);
+        User::truncate();
+        User::create([
+            'id' => 1,
+            'code' => 'SUPER ADMIN',
+        	'email' => 'ducthang.dt03@gmail.com',
+        	'password' => bcrypt('123123123'),
+        	'name' => 'Super Admin',
+        	'birthday' => '1999-03-21',
+        	'phone_number' => '0563047024',
+        	'address' => 'Đại học kiến trúc Hà Nội',
+        	'gender' => 'Nam',
+        ]);
     }
 }
